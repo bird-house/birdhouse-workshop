af74768df8b860ed5101d078676926e87e71fdb6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-19 10:55:51 +0200

    added fix for x11

--- demo/plotter.py
+++ demo/plotter.py
@@ -1,3 +1,8 @@
+import matplotlib
+# no X11 server ... must be run first
+# https://github.com/matplotlib/matplotlib/issues/3466/
+matplotlib.use('Agg')
+
 import matplotlib.pylab as plt
 import cartopy.crs as ccrs
 from netCDF4 import Dataset
