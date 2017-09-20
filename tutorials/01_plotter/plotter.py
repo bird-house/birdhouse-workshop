b63f2cb744c78ccee6ee130f494c7a6a4e2a404e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-20 14:47:37 +0200

    01_plotter tutorial added

---
+++ tutorials/01_plotter/plotter.py
@@ -0,0 +1,45 @@
+import matplotlib.pylab as plt
+# TODO: import ccrs for map projections
+# import cartopy.crs as ccrs
+from netCDF4 import Dataset
+
+AIR_DS = '../data/air.mon.ltm.nc'
+
+
+def simple_plot(resource, variable=None, output='plot.png'):
+    """
+    Generates a nice and simple plot.
+    """
+    print("Plotting {} ...".format(resource))
+
+    # Create dataset from resource ... a local NetCDF file or a remote OpenDAP URL
+    ds = Dataset(resource)
+
+    # Get the values of the given variable
+    values = ds.variables[variable]
+
+    # Prepare plot with a given size
+    fig = plt.figure(figsize=(20, 10))
+
+    # TODO: add projection
+    # ax = plt.axes(projection=ccrs.PlateCarree())
+
+    # Render a contour plot for the first timestep
+    plt.contourf(values[0, :, :])
+
+    # TODO: add background image with coastlines
+    # ax.stock_img()
+    # ax.coastlines()
+
+    # TODO: add a colorbar
+    # plt.colorbar()
+
+    # Save the plot to filesystem
+    fig.savefig(output)
+    plt.close()
+    print("Plot written to {}".format(output))
+    return output
+
+
+if __name__ == '__main__':
+    simple_plot(resource=AIR_DS, variable="air")
