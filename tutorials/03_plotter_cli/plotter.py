7568692a610495f8a788ca012f21c45b1dade123
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-20 18:22:32 +0200

    added projection parameter

--- tutorials/03_plotter_cli/plotter.py
+++ tutorials/03_plotter_cli/plotter.py
@@ -8,11 +8,13 @@ DATADIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'data')
 AIR_DS = os.path.join(DATADIR, 'air.mon.ltm.nc')
 
 
-def simple_plot(resource, variable=None, output='plot.png'):
+def simple_plot(resource, variable=None, projection=None, output='plot.png'):
     """
     Generates a nice and simple plot.
     """
     print("Plotting {} ...".format(resource))
+    projection = projection or ccrs.PlateCarree()
+    print("Using map projection {}".format(projection))
 
     # Create dataset from resource ... a local NetCDF file or a remote OpenDAP URL
     ds = Dataset(resource)
@@ -24,13 +26,14 @@ def simple_plot(resource, variable=None, output='plot.png'):
     fig = plt.figure(figsize=(20, 10))
 
     # add projection
-    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax = plt.axes(projection=projection)
 
     # Render a contour plot for the first timestep
     plt.contourf(values[0, :, :])
 
     # add background image with coastlines
     ax.stock_img()
+    # ax.set_global()
     ax.coastlines()
 
     # add a colorbar
@@ -65,8 +68,13 @@ if __name__ == '__main__':
                         help='a NetCDF file or an OpenDAP URL')
     parser.add_argument('-V', '--variable', nargs='?', default='air',
                         help='variable to plot (default: air)')
+    parser.add_argument('-p', '--projection', nargs='?', default='PlateCarree',
+                        choices=['PlateCarree', 'Mollweide', 'Robinson'],
+                        help='map projection (default: PlateCarree)')
 
     args = parser.parse_args()
     print("dataset={0.dataset}, variable={0.variable}".format(args))
-    output = simple_plot(resource=args.dataset[0], variable=args.variable)
+    # output = simple_plot(resource=args.dataset[0], variable=args.variable)
+    output = simple_plot(resource=args.dataset[0], variable=args.variable,
+                         projection=getattr(ccrs, args.projection)())
     print("Output: {}".format(output))
