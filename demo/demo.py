de5d3d9c699015f657ee58890a46ca350752c03d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-21 14:26:23 +0200

    fixed import

--- demo/demo.py
+++ demo/demo.py
@@ -20,13 +20,13 @@ def create_app(cfgfiles=None):
     """
     Creates PyWPS WSGI application.
     """
-    from demo.processes import processes
+    import processes
     config_files = [os.path.join(os.path.dirname(__file__), 'default.cfg')]
     if cfgfiles:
         config_files.extend(cfgfiles)
     if 'PYWPS_CFG' in os.environ:
         config_files.append(os.environ['PYWPS_CFG'])
-    service = Service(processes=processes, cfgfiles=config_files)
+    service = Service(processes=processes.processes, cfgfiles=config_files)
     return service
 
 
