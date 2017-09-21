e4ddc1d6e6b91070363cec1f5c1d85ef3ccb6060
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-09-21 17:05:32 +0200

    deactivated simple_plot

--- tutorials/10_plotter_process/processes/__init__.py
+++ tutorials/10_plotter_process/processes/__init__.py
@@ -1,9 +1,9 @@
 from .wps_sayhello import SayHello
 # TODO: import SimplePlot process class
-from .wps_simple_plot import SimplePlot
+# from .wps_simple_plot import SimplePlot
 
 processes = [
     SayHello(),
     # TODO: add SimplePlot process instance
-    SimplePlot(),
+    # SimplePlot(),
 ]
